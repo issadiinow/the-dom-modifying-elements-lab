1f85c2dba3a221f830e654ded9bf893bd7745217
Issa Mohamed <issa.mohamed@student.moringaschool.com>
2024-12-22 12:40:48 +0300

    completed dom modifying lab

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Write your code here!
+// 1. Remove the existing <main> element
+const mainElement = document.getElementById('main');
+mainElement.remove();
+
+// 2. Create a new <h1> element
+const newHeader = document.createElement('h1');
+
+// 3. Set the id of the <h1> element to 'victory'
+newHeader.id = 'victory';
+
+// 4. Set the text content of the <h1> element
+newHeader.textContent = 'YOUR-NAME is the champion';
+
+// 5. Append the new <h1> element to the DOM
+document.body.appendChild(newHeader);
